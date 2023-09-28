00f69c5f684d73778033e3420ed4d815f3dcb5f1
@loki157 <lokesh2001sankar@gmail.com>
2023-09-28 06:07:02 +0530

    i

--- src/assets/imageListData.js
+++ src/assets/imageListData.js
@@ -1,3 +1,13 @@
 import thekkathil from "../assets/images/image-list/ajith-thekkathil-_bSUKTKWmRM-unsplash.jpg";
+import govind from "../assets/images/image-list/govind-krishnan-pwPcYzyJTtY-unsplash (1).jpg";
+import noteme from "../assets/images/image-list/notme-XymJ3PWbWh8-unsplash.jpg";
+import sidhh from "assets/images/image-list";
+import thbo from "./../assets/images/image-list";
+import cpoom from "../../../assets/images/image-list";
+import tonSeba from "../../../assets/images/image-list";
 
-export const imageListData = [{ img: thekkathil, title: "thekkathil", row: 1 }];
+export const imageListData = [
+  { img: thekkathil, title: "thekkathil", row: 1, col: 2 },
+  { imhtitle: govind, title: "", row: 1, cl: 1 },
+  { img: noteme, title: "lo" },
+];
