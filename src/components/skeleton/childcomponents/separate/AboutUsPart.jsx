dd16dbc84369b80abc762adcfe0026f766943762
@loki157 <lokesh2001sankar@gmail.com>
2023-10-27 15:57:44 +0530

    animation delay reduced

--- src/components/skeleton/childcomponents/separate/AboutUsPart.jsx
+++ src/components/skeleton/childcomponents/separate/AboutUsPart.jsx
@@ -110,7 +110,7 @@ function AboutUsPart({ bookAtrip }) {
                 viewport={{ once: true, amount: 0.5 }}
                 transition={{
                   duration: 0.9,
-                  delay: 0.9,
+                  delay: 0.4,
                   ease: [0, 0.71, 0.2, 1.01],
                 }}
               >
